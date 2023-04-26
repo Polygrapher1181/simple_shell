d986c2c95fe9ace7ef6b3478647f192463d0d1a3
Polycarp <123014770+Polygrapher1181@users.noreply.github.com>
2023-04-26 18:38:27 +0300

    Update env_shellby.c

--- env_shellby.c
+++ env_shellby.c
@@ -78,7 +78,8 @@ int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
 	environ = new_environ;
 	environ[index] = new_value;
 	environ[index + 1] = NULL;
-        return (0);
+        
+	return (0);
 }
 
 /**

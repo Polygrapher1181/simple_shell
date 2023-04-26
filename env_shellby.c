aa024c6d53b9a92e723176ac9a16ba263ecf1322
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-26 07:47:47 -0700

    a C file implementing three built-in shell commands for a shell program

---
+++ env_shellby.c
@@ -0,0 +1,114 @@
+#include "carp.h"
+
+/**
+ * shellby_env - Prints the current environment variables.
+ * @args: An array of arguments passed to the shell.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: 0 on success, -1 on failure.
+ *
+ * Description: Prints each variable in the format 'variable'='value'.
+ */
+int shellby_env(char **args, char __attribute__((__unused__)) **front)
+{
+	int index;
+	char newline = '\n';
+
+	if (!environ)
+		return (-1);
+
+	for (index = 0; environ[index]; index++)
+	{
+		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
+		write(STDOUT_FILENO, &newline, 1);
+	}
+
+	return (0);
+}
+
+/**
+ * shellby_setenv - Changes or adds an environment variable to the PATH.
+ * @args: An array of arguments passed to the shell.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: 0 on success, -1 on failure.
+ *
+ * Description: Sets the value of the variable specified in args[1]
+ * to the value specified in args[2], or adds a new variable to the
+ * environment if it doesn't already exist.
+ */
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
+{
+	char **env_var = NULL, **new_environ, *new_value;
+	size_t size;
+	int index;
+
+	if (!args[0] || !args[1])
+		return (create_error(args, -1));
+
+	new_value = malloc(_strlen(args[0]) + 1 + _strlen(args[1]) + 1);
+	if (!new_value)
+		return (create_error(args, -1));
+	_strcpy(new_value, args[0]);
+	_strcat(new_value, "=");
+	_strcat(new_value, args[1]);
+
+	env_var = _getenv(args[0]);
+	if (env_var)
+	{
+		free(*env_var);
+		*env_var = new_value;
+		return (0);
+	}
+
+	for (size = 0; environ[size]; size++)
+		;
+
+	new_environ = malloc(sizeof(char *) * (size + 2));
+	if (!new_environ)
+	{
+		free(new_value);
+		return (create_error(args, -1));
+	}
+
+	for (index = 0; environ[index]; index++)
+		new_environ[index] = environ[index];
+
+	free(environ);
+	environ = new_environ;
+	environ[index] = new_value;
+	environ[index + 1] = NULL;
+
+	return (0);
+}
+
+/**
+ * shellby_unsetenv - Deletes an environment variable from the PATH.
+ * @args: An array of arguments passed to the shell.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: 0 on success, -1 on failure.
+ *
+ * Description: Removes the specified variable from the environment.
+ */
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front)
+{
+	char **env_var, **new_environ;
+	size_t size;
+	int index, index2;
+
+	if (!args[0])
+		return (create_error(args, -1));
+	env_var = _getenv(args[0]);
+	if (!env_var)
+		return (0);
+
+	for (size = 0; environ[size]; size++)
+		;
+
+	new_environ = malloc(sizeof(char *) * size);
+	if (!new_environ)
+		return (create_error(args, -1));
+
+	for (index = 0, index2 = 0; environ
+}
